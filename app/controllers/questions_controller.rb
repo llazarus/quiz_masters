58a89c41db36e81f49178861aef9671a51b961fc
Daniel Scholz <daniel.martin.scholz@gmail.com>
2018-11-17 12:45:31 -0800

    new question action finished, merged create and action into one action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,15 +4,27 @@ class QuestionsController < ApplicationController
     # description. It will only have the PK which is id.
     @question = Question.new
     @quiz = Quiz.find params[:quiz_id]
-
     @question.quiz = @quiz
-    @quiz.question.user = current_user
+    @question.user = current_user
+
+    if @quiz.user == current_user && @question.save
+      # render json: question
+      respond_to do |format|
+        format.html { render } # this will render `views/questions/index.html.erb`
+        format.json { render json: @question }
+      end
+      # TODO redirect/render will be unneccesary because of react. 
+      # redirect_to quiz_question_path(@question.id)
+    else 
+      # TODO same as above
+      flash[:danger] = "Unable to create question"
+    end
   end
 
   private
 
   def question_params
-    params.require(:question).permit(:body)
+    params.require(:question).permit(:title, :description)
   end
 
 
