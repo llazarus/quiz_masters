3171508aee8127a72b08ec533e8f559db3a30356
Daniel Scholz <daniel.martin.scholz@gmail.com>
2018-11-17 12:57:07 -0800

    added user references to question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_043046) do
+ActiveRecord::Schema.define(version: 2018_11_17_205331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,9 @@ ActiveRecord::Schema.define(version: 2018_11_17_043046) do
     t.bigint "quiz_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["quiz_id"], name: "index_questions_on_quiz_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "quizzes", force: :cascade do |t|
@@ -78,6 +80,7 @@ ActiveRecord::Schema.define(version: 2018_11_17_043046) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "quizzes"
+  add_foreign_key "questions", "users"
   add_foreign_key "quizzes", "users"
   add_foreign_key "takes", "quizzes"
   add_foreign_key "takes", "users"
