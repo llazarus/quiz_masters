aa33d4f6a0ef9479b56fd65ed735d7aeba20e98e
Daniel Scholz <daniel.martin.scholz@gmail.com>
2018-11-17 11:47:11 -0800

    Merged question create and question new into one action. As question create has no view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,19 @@
 class QuestionsController < ApplicationController
+  def new 
+    # Question will initially be created without title or
+    # description. It will only have the PK which is id.
+    @question = Question.new
+    @quiz = Quiz.find params[:quiz_id]
+
+    @question.quiz = @quiz
+    @quiz.question.user = current_user
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+
 end
