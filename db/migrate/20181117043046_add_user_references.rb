31569712ad2625f16f602e2ad74985fb0fd34a37
Ziggy Shea <zigs_17@hotmail.com>
2018-11-16 20:34:04 -0800

    Added user foreign keys to quizzes and takes

---
+++ db/migrate/20181117043046_add_user_references.rb
@@ -0,0 +1,6 @@
+class AddUserReferences < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :quizzes, :user, foreign_key: true
+    add_reference :takes, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_031958) do
+ActiveRecord::Schema.define(version: 2018_11_17_043046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.integer "points", default: 0, null: false
-    t.boolean "admin", default: false, null: false
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct"
@@ -61,6 +40,8 @@ ActiveRecord::Schema.define(version: 2018_11_17_031958) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
 
   create_table "takes", force: :cascade do |t|
@@ -69,10 +50,35 @@ ActiveRecord::Schema.define(version: 2018_11_17_031958) do
     t.integer "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["quiz_id"], name: "index_takes_on_quiz_id"
+    t.index ["user_id"], name: "index_takes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.integer "points", default: 0, null: false
+    t.boolean "admin", default: false, null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "quizzes"
+  add_foreign_key "quizzes", "users"
   add_foreign_key "takes", "quizzes"
+  add_foreign_key "takes", "users"
 end
