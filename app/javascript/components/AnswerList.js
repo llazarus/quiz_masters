62b1fd4e7bae6a7e134dbb83fd694ea5b1f9fc4d
Derek <derek.s.leung.407@gmail.com>
2018-11-17 16:01:00 -0800

    AnswerList renders, todo: connect it with controller data

--- app/javascript/components/AnswerList.js
+++ app/javascript/components/AnswerList.js
@@ -1,17 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import AnswerDetails from "./AnswerDetails"
 
-
+// This expects a prop called "answers".
 const AnswerList = (props) => (
   <section className="AnswerList">
     <ul>
       {props.answers
-      .map(answer=>(
-        <AnswerDetails 
-          {...answer}
-        />
-      ))
-    
+        .map(answer=>(
+          <AnswerDetails 
+            {...answer}
+          />
+        ))
       }
 
     </ul>
