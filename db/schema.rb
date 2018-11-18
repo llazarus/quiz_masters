1609d9a316a24089daba6e56e07e5ccb96a4e4c3
Leland Lazarus <llazarus93@gmail.com>
2018-11-18 12:22:09 -0800

    Dropped title field from questions table. Removed title field from _formQ question partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_205759) do
+ActiveRecord::Schema.define(version: 2018_11_18_201956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2018_11_17_205759) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
     t.text "description"
     t.bigint "quiz_id"
     t.datetime "created_at", null: false
