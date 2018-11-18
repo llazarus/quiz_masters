22f3c3f17dd50132ab877c01f602874189f59969
Daniel Scholz <daniel.martin.scholz@gmail.com>
2018-11-18 12:30:13 -0800

    Basic bootstrap navbar working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,3 @@
 class ApplicationController < ActionController::Base
 
-  private
-
-  def current_user
-    if session[:user_id].present?
-      @current_user ||= User.find(session[:user_id])
-    end
-  end
-  helper_method :current_user
-
-  def user_signed_in?
-    current_user.present?
-  end
-  helper_method :user_signed_in?
-
 end
