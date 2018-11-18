cf6bc41200d6322c0725909d8ef72c2aec78dbdd
Leland Lazarus <llazarus93@gmail.com>
2018-11-18 11:05:04 -0800

    Created necessary Takes routes and added takes#create, takes#update, and takes#destroy actions

--- app/controllers/takes_controller.rb
+++ app/controllers/takes_controller.rb
@@ -1,2 +1,27 @@
 class TakesController < ApplicationController
+  def create
+    @quiz = Quiz.find(params[:quiz_id])
+    @take = Take.new(
+      quiz_id: @quiz.id,
+      user_id: current_user.id,
+      score: 0,
+      attempts: 0
+    )
+
+    if @take.save
+      redirect_to quizzes_path
+    else
+      flash[:alert] = "Unable to begin quiz"
+      redirect_to root_path
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+    @take = Take.find(params[:id])
+    @take.destroy
+    redirect_to quizzes_path
+  end
 end
