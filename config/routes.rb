70bc07cce3c2aa782c12518b3803757e6436ed1d
Daniel Scholz <daniel.martin.scholz@gmail.com>
2018-11-18 11:17:14 -0800

    changed root route to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root to: "quizzes#index"
+  root to: "welcome#index"
   get('/leaderboard', { to: 'leaderboards#index'})
 
   resources :quizzes do
