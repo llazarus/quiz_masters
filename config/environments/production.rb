3621b89123a4a432e35ac027d76ab519d864b66a
Derek <derek.s.leung.407@gmail.com>
2018-12-18 20:48:28 -0800

    Added code to production environment to solve issues with mailer. Committing before a test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -76,6 +76,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: "https://inquizzitive.herokuapp.com/"
   }
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: "587",
+      enable_starttls_auto: true,
+      authentication: :plain,
+      domain: "https://inquizzitive.herokuapp.com/",
+      user_name: ENV["EMAIL_USERNAME"],
+      password: ENV["EMAIL_PASSWORD"],
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
